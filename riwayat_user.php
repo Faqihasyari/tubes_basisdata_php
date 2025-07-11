c6fd7f8d6dd9bf2132693ddd2827cf7aa5a635ea
Faqihasyari <faqih8158@gmail.com>
2025-07-11 22:40:51 +0700

    push

--- riwayat_user.php
+++ riwayat_user.php
@@ -22,19 +22,38 @@ $data = mysqli_query($conn, "SELECT p.*, b.judul FROM peminjaman p JOIN buku b O
 <html>
 <head>
   <title>Riwayat Peminjaman</title>
+  <style>
+    .terlambat {
+      background-color: #ffdddd;
+    }
+  </style>
 </head>
 <body>
 <h3>Riwayat Peminjaman Anda</h3>
 <a href="dashboard_user.php">Dashboard</a>
 <table border="1">
 <tr><th>Buku</th><th>Pinjam</th><th>Kembali</th><th>Status</th></tr>
-<?php while ($d = mysqli_fetch_array($data)) { ?>
-<tr>
+
+<?php while ($d = mysqli_fetch_array($data)) { 
+  // Cek apakah pinjaman sudah lewat 7 hari dan belum dikembalikan
+  $tanggal_pinjam = new DateTime($d['tanggal_pinjam']);
+  $tanggal_sekarang = new DateTime();
+  $interval = $tanggal_pinjam->diff($tanggal_sekarang)->days;
+  $jatuhTempo = ($interval > 7 && $d['status_pinjam'] == 'dipinjam');
+  ?>
+  
+<tr class="<?= $jatuhTempo ? 'terlambat' : '' ?>">
   <td><?= $d['judul'] ?></td>
   <td><?= $d['tanggal_pinjam'] ?></td>
-  <td><?= $d['tanggal_kembali'] ?></td>
-  <td><?= $d['status_pinjam'] ?></td>
+  <td><?= $d['tanggal_kembali'] ?? '-' ?></td>
+  <td>
+    <?= $d['status_pinjam'] ?>
+    <?php if ($jatuhTempo): ?>
+      <span style="color: red;">(Terlambat!)</span>
+    <?php endif; ?>
+  </td>
 </tr>
+
 <?php } ?>
 </table>
 </body>
